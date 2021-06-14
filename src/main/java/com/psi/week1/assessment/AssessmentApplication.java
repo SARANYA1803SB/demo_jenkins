c14ab439a905cb50855628171bfdea9337da0292
saranya <saranyasb2000@gmail.com>
2021-06-14 17:05:47 +0530

    assessment

--- src/main/java/com/psi/week1/assessment/AssessmentApplication.java
+++ src/main/java/com/psi/week1/assessment/AssessmentApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.psi.week1.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AssessmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AssessmentApplication.class, args);
 	}
 
 }
--- src/test/java/com/psi/week1/assessment/AssessmentApplicationTests.java
+++ src/test/java/com/psi/week1/assessment/AssessmentApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.psi.week1.assessment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AssessmentApplicationTests {
 
 	@Test
 	void contextLoads() {
